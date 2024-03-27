c7567d95c393ab78c3ce90349f3200625913c99a
Manpreet Gulati <designviacode@gmail.com>
2024-03-27 17:22:12 +0530

    sidebar buttons implemented & overall sidebar - ready

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,32 @@
 import { Button } from "./ui/button";
+import photoroomLogo from "../../public/photoroom-logo.png";
+import { Folder } from "lucide-react";
 
 const DEFAULT_FOLDER_NAME = "Untitled Folder";
 
 export const Sidebar = () => {
   return (
-    <div className="app-sidebar flex flex-col h-dvh w-[400px] border-l border-gray-200 bg-gray-100 p-5 pt-10 space-y-3">
+    <div className="app-sidebar flex flex-col h-dvh w-[350px] border-l border-gray-200 bg-gray-100 p-5 pt-10 space-y-3">
+      <img src={photoroomLogo} width={150} className="pb-2" />
+
       <Button className="w-full border border-slate-300 rounded-md p-2">
         New Folder
       </Button>
 
-      <div className="items-center text-left rounded-md p-3 pl-4 cursor-pointer hover:bg-slate-200">
-        <p className="text-sm font-semibold">{DEFAULT_FOLDER_NAME}</p>
-      </div>
+      <SidebarButton />
+      <SidebarButton text="Folder 2" />
+      <SidebarButton text="Folder 2" />
     </div>
   );
 };
+
+function SidebarButton({ text = DEFAULT_FOLDER_NAME }: { text?: string }) {
+  return (
+    <div className="group flex w-full items-center text-left rounded-md p-3 pl-4 cursor-pointer hover:bg-slate-200 space-x-3">
+      <Folder className="transition-all group-hover:text-blue-500 group-hover:translate-x-1" />
+      <p className="transition-all text-sm font-medium group-hover:text-blue-500 group-hover:font-semibold">
+        {text}
+      </p>
+    </div>
+  );
+}
